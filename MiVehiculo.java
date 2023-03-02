53720ba9a99813afb5f41a2ab219fc6a168a1254
reyessinai19 <124127368+reyessinai19@users.noreply.github.com>
2023-03-01 23:21:42 -0600

    Add files via upload

---
+++ MiVehiculo.java
@@ -0,0 +1,37 @@
+public class  vehiculo {
+     privateString name;
+     privateString marca;
+     privateString modelo;
+    privateString kilometraje;
+    
+    }
+    // ingresa tu vehiculo
+     vehiculo(String name, String marca, String modelo, String kilometraje){
+        setName(name);
+        setMarca(marca);
+        setModelo(modelo);
+        setKilometraje(kilometraje);
+        
+        }   
+
+        //las modificaciones 
+
+        public void setName(String n){name = n;}
+        public void setMarca(String a){marca = a;}
+        public void setmModelo(String b){modelo = b;}
+        public void setKilometraje(kilometraje h){kilometraje = h;}
+        
+        //Open
+
+        public String getName(){return name;}
+        public String getMarca(){return marca;}
+        public String getModelo(){return modelo}
+        public String getKilometraje(){return kilometraje;}
+
+        public void mostrarVehiculo (){
+            // TU VEHICULO 
+        
+        // Sinai Morales Reyes  2-ISC   Sabados, matutino
+
+
+        }
